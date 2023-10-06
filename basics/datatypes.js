bb9eb7e9473b0c2e6f87da1d46aebef5c480e508
Bhaskar Pandey <71315985+chikkibum@users.noreply.github.com>
2023-10-06 20:32:24 +0000

    datatypes notes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //use to treat all the js written as new
+
+//alert(3+3) won't run on nodejs , onoly on browser
+
+//use mdn docs
+
+//js has same primitive data types as other languages
+
+//object  (important)
+
+const details= {
+    name:"sumer",
+}
+
+console.log(details);
